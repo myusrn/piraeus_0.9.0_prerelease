9bbaf96e9f3acca5591fec7f235d983c511e295e
skunklab <theskunklab@gmail.com>
2017-03-03 09:22:36 -0600

    update choco install script

--- templates/chocoInstallScript.ps1
+++ templates/chocoInstallScript.ps1
@@ -1,1 +1,2 @@
-iwr https://chocolatey.org/install.ps1 -UseBasicParsing | iex
+iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1')) 
+choco install WindowsAzurePowershell
