103f8930af878e9d1229805994fbdb01d1800b32
skunklab <theskunklab@gmail.com>
2017-03-07 04:49:30 -0800

    update DSC

--- templates/Main.ps1
+++ templates/Main.ps1
@@ -71,35 +71,6 @@ Node $nodeName
     {
       Name = "Web-AppInit"
       Ensure = "Present"
-    }
-    Script DownloadWebDeploy
-    {
-        TestScript = {
-            Test-Path "C:\WindowsAzure\WebDeploy_amd64_en-US.msi"
-        }
-        SetScript ={
-            $source = "https://download.microsoft.com/download/0/1/D/01DC28EA-638C-4A22-A57B-4CEF97755C6C/WebDeploy_amd64_en-US.msi"
-            $dest = "C:\WindowsAzure\WebDeploy_amd64_en-US.msi"
-            Invoke-WebRequest $source -OutFile $dest
-        }
-        GetScript = {@{Result = "DownloadWebDeploy"}}
-        DependsOn = "[WindowsFeature]WebServerRole"
-    }
-    Package InstallWebDeploy
-    {
-        Ensure = "Present"  
-        Path  = "C:\WindowsAzure\WebDeploy_amd64_en-US.msi"
-        Name = "Microsoft Web Deploy 3.6"
-        ProductId = "{ED4CC1E5-043E-4157-8452-B5E533FE2BA1}"
-        Arguments = "ADDLOCAL=ALL"
-        DependsOn = "[Script]DownloadWebDeploy"
-    }
-    Service StartWebDeploy
-    {                    
-        Name = "WMSVC"
-        StartupType = "Automatic"
-        State = "Running"
-        DependsOn = "[Package]InstallWebDeploy"
-    } 
+    }    
   }
 }
