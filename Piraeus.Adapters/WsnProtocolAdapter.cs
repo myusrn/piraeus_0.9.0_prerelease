148f5448864ee3c57e8833811d2172c7dd0642db
skunklab <theskunklab@gmail.com>
2017-10-21 06:42:55 -0500

    remove adpater warning

---
+++ Piraeus.Adapters/WsnProtocolAdapter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SkunkLab.Channels;
+
+namespace Piraeus.Adapters
+{
+    public class WsnProtocolAdapter : ProtocolAdapter
+    {
+        public override IChannel Channel { get; set; }
+
+        public override event EventHandler<ProtocolAdapterErrorEventArgs> OnError;
+        public override event EventHandler<ProtocolAdapterCloseEventArgs> OnClose;
+        public override event EventHandler<ChannelObserverEventArgs> OnObserve;
+
+        public override void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Init()
+        {
+
+        }
+    }
+}
