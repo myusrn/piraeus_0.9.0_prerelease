5c341f79b4501f04c32ef30c08525a07794d0d0c
The Skunk Lab <theskunklab@gmail.com>
2018-02-02 17:22:09 -0600

    remove restart vm

--- scripts/piraeus_vm_config.ps1
+++ scripts/piraeus_vm_config.ps1
@@ -1,4 +1,3 @@
 Invoke-WebRequest "https://github.com/docker/compose/releases/download/1.18.0/docker-compose-Windows-x86_64.exe" -UseBasicParsing -OutFile $Env:ProgramFiles\docker\docker-compose.exe
 New-VMSwitch -name ExternalSwitch  -NetAdapterName "Ethernet 3" -AllowManagementOS $true
-Restart-Computer
 
