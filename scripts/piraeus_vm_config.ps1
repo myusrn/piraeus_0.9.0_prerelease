b489b0c72d2cd36bb2a2ad025b69b072f1e58dd5
The Skunk Lab <theskunklab@gmail.com>
2018-02-02 11:25:46 -0600

    Update piraeus_vm_config.ps1

--- scripts/piraeus_vm_config.ps1
+++ scripts/piraeus_vm_config.ps1
@@ -1,3 +1,4 @@
-﻿Invoke-WebRequest "https://github.com/docker/compose/releases/download/1.18.0/docker-compose-Windows-x86_64.exe" -UseBasicParsing -OutFile $Env:ProgramFiles\docker\docker-compose.exe
+Invoke-WebRequest "https://github.com/docker/compose/releases/download/1.18.0/docker-compose-Windows-x86_64.exe" -UseBasicParsing -OutFile $Env:ProgramFiles\docker\docker-compose.exe
 New-VMSwitch -name ExternalSwitch  -NetAdapterName "Ethernet 3" -AllowManagementOS $true
+Restart-Computer
 
