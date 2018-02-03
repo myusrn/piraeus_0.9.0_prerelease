97427043921b107cb31dd9e4b6dc685a9ebe4c93
The Skunk Lab <theskunklab@gmail.com>
2018-02-02 18:21:11 -0600

    add docker pull for images

--- scripts/piraeus_vm_config.ps1
+++ scripts/piraeus_vm_config.ps1
@@ -1,3 +1,6 @@
 Invoke-WebRequest "https://github.com/docker/compose/releases/download/1.18.0/docker-compose-Windows-x86_64.exe" -UseBasicParsing -OutFile $Env:ProgramFiles\docker\docker-compose.exe
+docker pull skunklab/orleans-silo
+docker pull skunklab/tcpudpgateway
+docker pull skunklab/webgateway
 New-VMSwitch -name ExternalSwitch  -NetAdapterName "Ethernet 3" -AllowManagementOS $true
 
